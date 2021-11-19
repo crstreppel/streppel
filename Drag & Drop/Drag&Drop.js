880e9eef6a8eea518463774f0afd1a8293808a60
Claudio Ricardo Streppel <streppel@gmail.com>
2021-11-19 18:02:48 -0300

    drag e drop

---
+++ Drag & Drop/Drag&Drop.js
@@ -0,0 +1,28 @@
+// function dragStart(event){
+//    event.dataTransfer.setData('text', event.target.id);
+// }
+// function allowDrop(event){
+//         event.preventDefault();
+// }
+
+// function drop(event){
+//     var imageId = event.dataTransfer.getdata('text')
+//     var image = document.getElementById(imageId)
+//     event.target.appendChild(image)
+    
+// }
+
+function dragStart(event){
+	event.dataTransfer.setData('text', event.target.id);
+}
+
+function allowDrop(event){
+	event.preventDefault();
+}
+
+function drop(event){
+   var imageId = event.dataTransfer.getData('text'),
+   image = document.getElementById(imageId);
+
+   event.target.appendChild(image);
+}
