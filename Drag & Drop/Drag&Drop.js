82cd6780bbe505c7b29874eb37620e9ecd4efc96
Claudio Ricardo Streppel <streppel@gmail.com>
2021-11-22 14:51:13 -0300

    grag e drop

--- Drag & Drop/Drag&Drop.js
+++ Drag & Drop/Drag&Drop.js
@@ -1,16 +1,4 @@
-// function dragStart(event){
-//    event.dataTransfer.setData('text', event.target.id);
-// }
-// function allowDrop(event){
-//         event.preventDefault();
-// }
 
-// function drop(event){
-//     var imageId = event.dataTransfer.getdata('text')
-//     var image = document.getElementById(imageId)
-//     event.target.appendChild(image)
-    
-// }
 
 function dragStart(event){
 	event.dataTransfer.setData('text', event.target.id);
